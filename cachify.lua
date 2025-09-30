81c23827b8931d0dfccc0e1f2c24d8ad2768d539
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-09-29 21:07:56 -0300

    comand line entries

--- cachify.lua
+++ cachify.lua
@@ -1,1 +1,13 @@
 print("mensagem de teste")
+local total_sources = argv.get_flag_size({ "src", "sources" })
+if total_sources == 0 then
+    print("No sources provided. Use --src <source1> <source2> ...")
+    return 
+end
+ local cmd = argv.get_flag_arg_by_index({ "cmd" }, 1)
+ if not cmd then
+        print("No command provided. Use --cmd <command>")
+        return
+end
+
+
