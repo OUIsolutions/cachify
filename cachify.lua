18f581e325ab7b0494d27bc9a49332e8056d1054
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-09-29 21:20:22 -0300

    att

--- cachify.lua
+++ cachify.lua
@@ -10,4 +10,26 @@ end
         return
 end
 
+local mode = argv.get_flag_arg_by_index({ "mode" }, 1) or "last_modification"
+if mode ~= "last_modification" and mode ~= "content" then
+    print("Invalid --mode. Use 'last_modification' or 'content'.")
+    return
+end
 
+local hasher = dtw.newHasher()
+
+for i=1,total_sources do 
+    local current = argv.get_flag_arg_by_index({ "src", "sources" }, i)
+    if dtw.isfile(current) then 
+            hasher.digest_file(current)
+    end 
+    if dtw.isdir(current) then 
+            if mode == "last_modification" then 
+                hasher.digest_folder_by_last_modification(current)
+            end 
+            if mode == "content" then 
+                hasher.digest_folder_by_content(current)
+            end
+    end
+
+end 
